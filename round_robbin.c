15a83751c75811e95235e3e47326350db3041a78
voonshunzhi <38502065+voonshunzhi@users.noreply.github.com>
2019-12-12 01:19:37 +0800

    Working round robin that takes in idle time

--- round_robbin.c
+++ round_robbin.c
@@ -1,113 +1,136 @@
-#include <stdio.h>
-#include <time.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int find_turnaround(int arrival_time, int completion);
 int find_waiting_time(int turnaround, int burst_time);
-void print_output(int turnaround, int waiting_time, int process);
+void copyArray(int a[],int burstTime[],int noOfProcesses);
 
-int main(){
-    
-    // Declaring variables
-    int time_quantum = 5, tracking_counter = 0;
-    int burst_time, arrival_time, waiting_time, turnaround_time;
+int main()
+{
+    int choice;
+    int count, number_of_processes, total_time, remain, completed = 0, id=0;
+    int total_wait_time = 0,total_turnaround_time = 0;
+    int ids[10][10];
 
-    //Arrays to store different values
-    int arrival_array_asc[100] = {0,3,4,8};
+    int arrival_time[] = {5,16,25,34,44};
+    int ascendingBurstTime[] = {9,25,36,42,100};
+    int descendingBurstTime[] = {55,42,36,25,18};
+    int remaining_time[10];
+    int time_quantum;
 
-    // Ascending and Descending order dummy data
-    int burst_array_asc[] = {6,7,8,9};
+    int length = sizeof(arrival_time) / sizeof(arrival_time[0]);
+    number_of_processes = length;
+    remain=number_of_processes;
+    int burst_time[number_of_processes];
 
-    // To find the number of items in the array
-    int input_arraySize = sizeof(burst_array_asc)/ sizeof(burst_array_asc[0]);
+    printf("Press 1 for ascending workload or Press 2 for descending workload\n");
+    printf("Enter your choice :");
+    scanf("%d",&choice);
 
-    // Copy arrival array
-    int copy_arrival_array[100];
-    int copy_burst_array[100];
-    int process_ids[100];
-
-    for(int i = 0; i < input_arraySize; i++) {
-      copy_arrival_array[i] = arrival_array_asc[i];
-      copy_burst_array[i] = burst_array_asc[i];
-      process_ids[i] = i+1;
+    if(choice == 1)
+    {
+        //assign burst time with ascending order workload
+        copyArray(ascendingBurstTime,burst_time,number_of_processes);
+    }
+    else
+    {
+        //assign burst time with descending order workload
+        copyArray(descendingBurstTime,burst_time,number_of_processes);
     }
 
-        int count = 0;
-        int num_of_process_pending = 0;
-        int dynamic_array_length = 0;
-        int exceed[1000];
+    printf("Enter a suitable time quantum :");
+    scanf("%d",&time_quantum);
+
+  for(int i = 0; i < length; i++)
+    {
+        // Remainig time is a copy of burst time to keep burst time intact
+        remaining_time[i] = burst_time[i];
+    }
+
+    printf(" _________________________________________________________________________________\n");
+    printf("|           |                |                |                |                  |\n");
+    printf("|%s |%14s  |%13s   |%14s  |%17s |\n"," Processes","Arrival Time","Burst time","Waiting time","Turn around time");
+    printf("|___________|________________|________________|________________|__________________|\n");
+    printf("|           |                |                |                |                  |\n");
+
+
+  for(total_time= arrival_time[0], count=0; remain!= 0;)
+  {
+    // if remaining_time[count] == 0; meaning done
+    if(remaining_time[count] <= time_quantum && remaining_time[count] > 0)
+    {   total_time += remaining_time[count];
+      remaining_time[count]=0;
+      completed = 1;
+    }
+    else if(remaining_time[count]>0)
+    {
+      remaining_time[count]-=time_quantum;
+      total_time+=time_quantum;
+    }
+    if(remaining_time[count]==0 && completed == 1)
+    {
+      remain--;
+      int turnaround = find_turnaround(arrival_time[count], total_time);
+      int waiting = find_waiting_time(turnaround, burst_time[count]);
+      total_wait_time += waiting;
+      total_turnaround_time += turnaround;
+      ids[count+1][0] = arrival_time[count];
+      ids[count+1][1] = burst_time[count];
+      ids[count+1][2] = waiting;
+      ids[count+1][3] = turnaround;
+      completed = 0;
+    }
     
+    
+    if(count == number_of_processes-1)
+      //If reach the end of array reset to 0
+      count=0;
+    else if(arrival_time[count+1] <= total_time)
+      count++;
+      
+    else if(arrival_time[count+1] > total_time) {
+        // if next process havent arrive yet, add the difference find_turnaround
+        total_time += arrival_time[count+1] - total_time;
+    }
+    
+    else
+      // if next element havent arrived yet go back to first element and run again
+      count=0;
+      
+  }
 
-        for(int i= 0;i<input_arraySize + dynamic_array_length; i++){
-            int remaining_burst = 0; 
-            int n = 0;
+  for(int i = 1; i <= number_of_processes; i++) {
+      printf("|     %d     |",i);
+      printf("%9d       |", ids[i][0]);
+      printf("%10d      |", ids[i][1]);
+      printf("%10d      |",ids[i][2]);
+      printf("%10d        |\n",ids[i][3]);
+  }
 
-            if(burst_array_asc[i] > 0) {
-              if (burst_array_asc[i] > time_quantum){
-                remaining_burst = burst_array_asc[i] - time_quantum;
-                tracking_counter += time_quantum;
-                num_of_process_pending++;
-                dynamic_array_length++;
-                
-                for(int j = i+1; j < input_arraySize; j++){
-                  if(tracking_counter > arrival_array_asc[j] && arrival_array_asc[j] != 0) {
-                    n++;
-                  }
-                }
+  printf("|___________|________________|________________|________________|__________________|\n");
 
+  printf("\nAverage Waiting Time= %f", (float)total_wait_time/number_of_processes);
+  printf("\nAvg Turnaround Time = %f", (float)total_turnaround_time/number_of_processes);
+  printf("\nThroughput = %f\n", (float) number_of_processes / total_time);
 
-                int c =  i + n + num_of_process_pending;
-
-                for(int i = (input_arraySize - 1) + dynamic_array_length; c < i; i--) {
-                  burst_array_asc[i] = burst_array_asc[i-1];
-                  copy_arrival_array[i] = copy_arrival_array[i-1];
-                  copy_burst_array[i] = copy_burst_array[i-1];
-                  process_ids[i] =  process_ids[i-1];
-                }
-
-                burst_array_asc[c] = remaining_burst;
-                copy_arrival_array[c] = copy_arrival_array[i];
-                copy_burst_array[c] = copy_burst_array[i];
-                 process_ids[c] =  process_ids[i];
-                exceed[c] = true;
-                burst_array_asc[i] = 0;
-
-              }
-
-            // Burst time is within time quantum
-              else {
-                tracking_counter += burst_array_asc[i];
-                burst_array_asc[i] = 0;
-                if(exceed[i] == true) {
-                  num_of_process_pending--;
-                }
-
-                int turnaround = find_turnaround(copy_arrival_array[i], tracking_counter);
-
-                int waiting = find_waiting_time(turnaround, copy_burst_array[i]);
-
-                print_output(turnaround, waiting,  process_ids[i]);
-              }
-            }
-
-
-              }
+  return 0;
 }
 
 int find_turnaround(int arrival_time, int completion){
     int turnaround = completion - arrival_time;
-
     return turnaround;
 }
 
 int find_waiting_time(int turnaround, int burst_time){
     int waiting_time = (turnaround - burst_time);
-
     return waiting_time;
 }
 
-void print_output(int turnaround, int waiting_time, int process){
-    printf("Process %d\n", process);
-    printf("Turnaround time is: %d\nWaiting time is: %d\n\n", turnaround, waiting_time); 
+//function to copy elements of an array to another array
+void copyArray(int a[],int burstTime[],int number_of_processes)
+{
+    for (int i=0 ;i<number_of_processes ;i++)
+    {
+        burstTime[i] = a[i];
+    }
 }
